eb29bb99dc40e3b57b86887419bc5b1d38004d04
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-04-09 10:48:33 +0200

    fixed the bootRun bug by removing duplication at build gradle file and setting and composing docker

---
+++ src/main/java/se/devnewsproject/App.java
@@ -0,0 +1,12 @@
+package se.devnewsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// This annotation is adding functionality
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
