17dc666ed5cc78567d13af8bdd8ff4a4304b696b
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-04-09 11:17:06 +0200

    coded Article class

--- src/main/java/se/devnewsproject/Article.java
+++ src/main/java/se/devnewsproject/Article.java
@@ -8,7 +8,43 @@ import javax.persistence.Id;
 public class Article {
 
     // unique id, title, body (article text content) and the authorName
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    
+    private String title;
+    private String body;
+    private String authorName;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
 }
