0a5588d04fb90a5cfb1581b0b8377f0233594f7e
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-04-09 11:39:56 +0200

    coded Article POST and GET

--- src/main/java/se/devnewsproject/ArticleController.java
+++ src/main/java/se/devnewsproject/ArticleController.java
@@ -1,4 +1,51 @@
 package se.devnewsproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public class ArticleController {
+
+    ArticleRepository articleRepository;
+
+    // ? is this what connects the repo-interface to this controller?
+    @Autowired
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    //get all articles
+    @GetMapping("/articles")
+    public List<Article> listAllArticles(){
+        List<Article> article = articleRepository.findAll();
+        return article;
+    }
+
+
+    // get specific article based on the provided id
+
+    // post a new article
+    // 1. call spring Class 'ResponseEntity'
+    // todo practice: I just remembered the 'save' part.
+    @PostMapping ("/articles")
+    public ResponseEntity<Article> createArticle(@RequestBody Article article) {
+        // call repository and save the article-external-input there
+        // todo NOTE: you don't need to create an object when posting
+        articleRepository.save(article);
+        // connect the output to a HTTP response
+        // ? why does ResponseEntity needs to be in the method declaration?
+        return ResponseEntity.status(HttpStatus.CREATED).body(article);
+    }
+
+
+}
+
+    // update the given article
+
+    // delete the given article
 }
--- src/main/java/se/devnewsproject/ArticleRepository.java
+++ src/main/java/se/devnewsproject/ArticleRepository.java
@@ -1,4 +1,7 @@
 package se.devnewsproject;
 
-public interface ArticleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
